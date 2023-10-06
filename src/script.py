0a87be0d3e0639b828b0d06074834c78abc01ee6
Manuel De Alba <ferman5903@gmail.com>
2023-10-05 21:36:04 -0600

    Buscar libros por campos, mensajes de error

--- src/script.py
+++ src/script.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from tkinter.ttk import *
+from tkinter.messagebox import showerror
 
 from Biblioteca import *
 from Libro import *
@@ -83,6 +85,8 @@ def abrirVentanaEliminar():
         if borrado:
             cerrar()
             actualizar_tabla()
+        else:
+            showerror(title="Libro no encontrado", message="Libro no encontrado")
 
     def cerrar():
         ventana_eliminar.destroy()
@@ -92,21 +96,66 @@ def abrirVentanaEliminar():
     boton_cancelar = Button(ventana_eliminar, text="Cancelar", command=cerrar)
     boton_cancelar.grid(row=2, column=2)
 
+def abrirVentanaBuscar():
+    ventana_buscar = Toplevel(root)
+    ventana_buscar.title("Buscar libro")
+
+    # Tipo de busqueda
+    label_tipo = Label(ventana_buscar, text="Tipo").grid(row=1, column=1)
+    combobox_tipo = Combobox(ventana_buscar,
+                        values=["Titulo", "Autor", "Genero", "Publicacion", "Estado"],
+                        state="readonly")
+    combobox_tipo.grid(row=1, column=2)
+    label_contenido = Label(ventana_buscar, text="Contenido").grid(row=2, column=1)
+    input_contenido = Entry(ventana_buscar)
+    input_contenido.grid(row=2, column=2)
+
+    def buscar():
+        encontrados = biblioteca.buscarLibros(combobox_tipo.get(), input_contenido.get())
+
+        if len(encontrados) > 0:
+            # Si se encontraron libros, se crea una tabla y se abre una ventana
+            ventana_encontrados = Toplevel(ventana_buscar)
+            ventana_encontrados.title("Libros encontrados")
+            
+            # Por cada libro, se obtiene su información y se le agrega el indice
+            info = []
+            for indice, libro in enumerate(encontrados):
+                datos_libro = libro.obtenerInformacion().split("\n")
+                info.append(["Indice: " + str(indice), *datos_libro])
+
+            tabla_encontrados = Tabla(ventana_encontrados, info).pack()
+
+            boton_salir = Button(ventana_encontrados, text="Salir", command=ventana_encontrados.destroy).pack()
+        else:
+            showerror(title="No se encontraron libros", message="No se encontraron libros")
+
+    def cerrar():
+        ventana_buscar.destroy()
+
+    boton_aceptar = Button(ventana_buscar, text="Buscar", command=buscar)
+    boton_aceptar.grid(row=3, column=1)
+    boton_cancelar = Button(ventana_buscar, text="Cancelar", command=cerrar)
+    boton_cancelar.grid(row=3, column=2)
+
 # Agregar libro (Boton que abre otra ventana)
 boton_agregar = Button(frame_botones, text="Agregar libro", command=abrirVentanaAgregar)
 boton_agregar.grid(row=1, column=1)
 
-#! Buscar libros
+# Buscar libros
+boton_buscar = Button(frame_botones, text="Buscar libro", command=abrirVentanaBuscar)
+boton_buscar.grid(row=1, column=2)
+
 #! Reservar libro
 #! Cancelar reserva de libro
 #! Editar información de libro
 #! Eliminar libro
 boton_eliminar = Button(frame_botones, text="Borrar libro", command=abrirVentanaEliminar)
-boton_eliminar.grid(row=1, column=2)
+boton_eliminar.grid(row=1, column=3)
 
 # Salir
 boton_salir = Button(frame_botones, text="Salir", command=lambda: root.destroy())
-boton_salir.grid(row=1, column=3)
+boton_salir.grid(row=1, column=4)
 
 root.mainloop()
 #     print("Eliminar libro\n")

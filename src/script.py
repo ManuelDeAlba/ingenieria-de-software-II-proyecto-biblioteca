e732313bee60425539c5ba97a7e44a7248eb85fa
Manuel De Alba <ferman5903@gmail.com>
2023-10-06 19:10:35 -0600

    Mejora en la eliminacion de libros

--- src/script.py
+++ src/script.py
@@ -17,7 +17,7 @@ Label(root, text="Biblioteca", font=('Arial', 16, 'bold')).pack(pady=10)
 
 # Mostrar libros (Tabla con los libros)
 libros = biblioteca.obtenerLibros()
-datos_tabla = [["Indice: " + str(x), *y] for x, y in enumerate(libros)]
+datos_tabla = [["Indice: " + str(x + 1), *y] for x, y in enumerate(libros)]
 tabla = Tabla(root, datos_tabla)
 tabla.pack()
 
@@ -28,7 +28,7 @@ frame_botones.pack(pady=10)
 # Para obtener la lista actualizada de libros y mostrarlos
 def actualizar_tabla():
     libros = biblioteca.obtenerLibros()
-    datos_tabla = [["Indice: " + str(x), *y] for x, y in enumerate(libros)]
+    datos_tabla = [["Indice: " + str(x + 1), *y] for x, y in enumerate(libros)]
     tabla.actualizar(datos_tabla)
 
 # Funciones para abrir las ventanas
@@ -70,32 +70,6 @@ def abrirVentanaAgregar():
     boton_cancelar = Button(ventana_agregar, text="Cancelar", command=lambda: ventana_agregar.destroy())
     boton_cancelar.grid(row=5, column=2)
 
-def abrirVentanaEliminar():
-    ventana_eliminar = Toplevel(root)
-    ventana_eliminar.title("Eliminar libro")
-
-    # Input para poner el nombre del libro a eliminar
-    label_titulo = Label(ventana_eliminar, text="Titulo").grid(row=1, column=1)
-    input_titulo = Entry(ventana_eliminar)
-    input_titulo.grid(row=1, column=2)
-
-    def aceptar():
-        borrado = biblioteca.eliminarLibro(input_titulo.get())
-
-        if borrado:
-            cerrar()
-            actualizar_tabla()
-        else:
-            showerror(title="Libro no encontrado", message="Libro no encontrado")
-
-    def cerrar():
-        ventana_eliminar.destroy()
-
-    boton_aceptar = Button(ventana_eliminar, text="Aceptar", command=aceptar)
-    boton_aceptar.grid(row=2, column=1)
-    boton_cancelar = Button(ventana_eliminar, text="Cancelar", command=cerrar)
-    boton_cancelar.grid(row=2, column=2)
-
 def abrirVentanaBuscar():
     ventana_buscar = Toplevel(root)
     ventana_buscar.title("Buscar libro")
@@ -122,21 +96,75 @@ def abrirVentanaBuscar():
             info = []
             for indice, libro in enumerate(encontrados):
                 datos_libro = libro.obtenerInformacion().split("\n")
-                info.append(["Indice: " + str(indice), *datos_libro])
+                info.append(["Indice: " + str(indice + 1), *datos_libro])
 
             tabla_encontrados = Tabla(ventana_encontrados, info).pack()
 
             boton_salir = Button(ventana_encontrados, text="Salir", command=ventana_encontrados.destroy).pack()
         else:
             showerror(title="No se encontraron libros", message="No se encontraron libros")
-
-    def cerrar():
-        ventana_buscar.destroy()
+            ventana_buscar.deiconify()
 
     boton_aceptar = Button(ventana_buscar, text="Buscar", command=buscar)
     boton_aceptar.grid(row=3, column=1)
-    boton_cancelar = Button(ventana_buscar, text="Cancelar", command=cerrar)
+    boton_cancelar = Button(ventana_buscar, text="Cancelar", command=ventana_buscar.destroy)
     boton_cancelar.grid(row=3, column=2)
+
+def abrirVentanaEliminar():
+    ventana_eliminar = Toplevel(root)
+    ventana_eliminar.title("Eliminar libro")
+
+    # Input para poner el nombre del libro a eliminar
+    label_titulo = Label(ventana_eliminar, text="Titulo").grid(row=1, column=1)
+    input_titulo = Entry(ventana_eliminar)
+    input_titulo.grid(row=1, column=2)
+
+    # Hace la búsqueda de las opciones de libros a eliminar
+    def aceptar():
+        encontrados = biblioteca.buscarLibros("titulo", input_titulo.get())
+
+        if len(encontrados):
+            # Se abre otra ventana con todos los libros encontrados
+            ventana_encontrados = Toplevel(ventana_eliminar)
+            ventana_encontrados.title("Libros encontrados")
+
+            # Tabla con los libros encontrados
+            info = []
+            for indice, libro in enumerate(encontrados):
+                datos_libro = libro.obtenerInformacion().split("\n")
+                info.append(["Indice: " + str(indice + 1), *datos_libro])
+            
+            tabla = Tabla(ventana_encontrados, info)
+            tabla.pack()
+
+            # Input para introducir el indice del libro que se quiere borrar
+            frame_botones = Frame(ventana_encontrados)
+            frame_botones.pack()
+            
+            label_indice = Label(frame_botones, text="Indice:")
+            label_indice.grid(row=0, column=0)
+            input_indice = Entry(frame_botones)
+            input_indice.grid(row=0, column=1)
+
+            def borrar():
+                # Se borra el libro con el indice seleccionado
+                biblioteca.eliminarLibro(encontrados[int(input_indice.get()) - 1])
+
+                ventana_encontrados.destroy()
+                actualizar_tabla()
+
+            boton_borrar = Button(frame_botones, text="Borrar", command=borrar)
+            boton_borrar.grid(row=1, column=0)
+            boton_cancelar = Button(frame_botones, text="Cancelar", command=ventana_encontrados.destroy)
+            boton_cancelar.grid(row=1, column=1)
+        else:
+            showerror(title="Libro no encontrado", message="Libro no encontrado")
+            ventana_eliminar.deiconify() # Para evitar que con el error se minimice la ventana
+
+    boton_aceptar = Button(ventana_eliminar, text="Aceptar", command=aceptar)
+    boton_aceptar.grid(row=2, column=1)
+    boton_cancelar = Button(ventana_eliminar, text="Cancelar", command=ventana_eliminar.destroy)
+    boton_cancelar.grid(row=2, column=2)
 
 # Agregar libro (Boton que abre otra ventana)
 boton_agregar = Button(frame_botones, text="Agregar libro", command=abrirVentanaAgregar)
@@ -149,7 +177,7 @@ boton_buscar.grid(row=1, column=2)
 #! Reservar libro
 #! Cancelar reserva de libro
 #! Editar información de libro
-#! Eliminar libro
+# Eliminar libro
 boton_eliminar = Button(frame_botones, text="Borrar libro", command=abrirVentanaEliminar)
 boton_eliminar.grid(row=1, column=3)
 
